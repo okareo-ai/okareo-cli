2fdcb5450d6c83a0110292101770c60e19d84cd2
Matt Wyman <matt@okareo.com>
2024-04-24 11:59:57 -0700

    update to version

--- cmd/root.go
+++ cmd/root.go
@@ -21,7 +21,7 @@ To use the CLI, refer to the docs: https://docs.okareo.com/docs/sdk/cli`,
 	Run: func(cmd *cobra.Command, args []string) {
 		vFull, _ := cmd.Flags().GetBool("version")
 		if vFull {
-			fmt.Println("v0.0.9")
+			fmt.Println("v0.0.10")
 		}
 	},
 }
